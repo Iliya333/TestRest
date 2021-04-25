b748ad2f953cdc2dc2c5a6c541a711cb0569a1f9
Iliya333 <69747853+Iliya333@users.noreply.github.com>
2021-04-25 21:34:22 +0500

    commit

--- src/main/java/com/service/DepartmentServiceImp.java
+++ src/main/java/com/service/DepartmentServiceImp.java
@@ -5,9 +5,11 @@ import com.exception.NotFoundException;
 import com.model.Department;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
+@Transactional
 public class DepartmentServiceImp implements DepartmentService {
 
 
--- src/main/java/com/service/EmployeeServiceImp.java
+++ src/main/java/com/service/EmployeeServiceImp.java
@@ -5,9 +5,11 @@ import com.exception.NotFoundException;
 import com.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 @Service
+@Transactional
 public class EmployeeServiceImp implements EmployeeService {
 
     private EmployeeDao employeeDao;
